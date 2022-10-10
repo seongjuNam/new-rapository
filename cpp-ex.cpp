b25b3494ab8549c409ecdb0d1b1be2695c89ae80
seongjuNam <nsj7727@naver.com>
2022-10-10 23:35:24 +0900

    cpp

---
+++ cpp-ex.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int grade;
+    cout << "나이출력 프로그램" << endl << endl;
+
+    cout << "나이를 입력하세요: ";
+    cin >> grade;
+
+    cout << "당신의 나이는" << grade <<"입니다.";
+    return 0;
+}
